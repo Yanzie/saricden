951598585c80fe6f10791841589d9f96a20b2c30
Kirk Morris <kmorris@sylsft.com>
2014-08-12 12:14:09 -0700

    Removes test output from util send view function.

--- src/php/sdUtils.class.php
+++ src/php/sdUtils.class.php
@@ -5,7 +5,6 @@ class sdUtils
     {
         $userAgent = $_SERVER['HTTP_USER_AGENT'];
         mail('kirk@saricden.com', 'site view', 'Someone went to saricden.com\n\nTheir user agent:\n'.$userAgent);
-        echo "test";
     }
 }
 ?>
